1112ca271ebf46fec76ba3466b45516f63db91d5
robertiki2001 <robertrlm16@gmail.com>
2023-12-18 20:19:48 +0100

    fixing serie-details

--- src/app/componentes/series-details/series-details.component.ts
+++ src/app/componentes/series-details/series-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../servicios/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-series-details',
@@ -9,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class SeriesDetailsComponent {
 
+  serie: any;
+
+  constructor(private route: ActivatedRoute,private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const characterId = params['id'];
+      this.apiService.getSeriePorId(characterId).subscribe({
+        next: (character: any) => {
+          this.serie = character;
+        },
+        error: (error: any) => {
+          console.error('Error loading character:', error);
+        }
+      });
+    });
+  }
 }
